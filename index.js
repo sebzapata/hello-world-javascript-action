1f2952fdc655962af3fc0d352ec76756a6d1a307
Sebastian Zapata <sebastian.zapata@bri53759l.hq.peninsula-uk.local>
2025-03-28 14:19:22 +0000

    add file list

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ try {
   const exec = util.promisify(require("child_process").exec);
   const workingDirectory = "../app";
 
+  fs.readdir(".", function (err, files) {
+    //handling error
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      console.log(file);
+    });
+  });
+
   console.log("starting the code");
   console.log("");
 
